99ac17b23e13102b564e7f14c080383fe17e0991
blackbronze <blackbronze@gmail.com>
2023-02-22 18:25:36 +0900

    vue 가이드 코드

--- main.js
+++ main.js
@@ -1,10 +1,36 @@
 const app = Vue.createApp({
     data() {
         return {
+
             product: 'Socks',
             image: './assets/images/socks_blue.jpg',
             url: 'http://www.naver.com',
-            inStock: false
+            inStock: true,
+
+            inventory: 8,
+            details: ['50% cotten', '30% wool', '20% polyester'],
+            variants: [
+                { id: 2234, color: 'green' },
+                { id: 2235, color: 'blue'}
+            ],
+            cart: 0,
+            vimgs: [
+                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
+                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' }
+            ]
+        }
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1;
+        },
+        removeFromCart() {
+            if(this.cart>0) {
+                this.cart -= 1;
+            }
+        },
+        updateImage(vimg) {
+            this.image = vimg;
         }
     }
 })
